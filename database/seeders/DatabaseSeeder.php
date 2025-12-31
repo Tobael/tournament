b7f2ad7c5d24f72293cafd4eafd8eeffb83bb445
Michael Strobel <mstrobel97@gmail.com>
2025-12-31 14:33:38 +0100

    fix action workflows

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->admin()->create([
+        User::factory()->admin()->withoutTwoFactor()->create([
             'name' => 'Admin',
             'email' => 'admin@tournaments.com',
         ]);
