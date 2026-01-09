3375c771055cf49a1fd077f49b41d097bf27b21c
Eixix <tobiasbetz@hotmail.de>
2026-01-09 23:40:42 +0100

    Repaired .env.example

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\Tournament;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('tournament.{id}', function ($user, $id) {
+    return Tournament::find($id)->users()->where('id', $user->id)->exists();
 });
